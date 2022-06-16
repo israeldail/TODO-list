881c482127cf0ee83e2150068b6231ad6a5dab97
izzy417 <israeldail2@gmail.com>
2022-06-16 00:46:09 +0000

    some js

--- src/js/component/body-list.jsx
+++ src/js/component/body-list.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 export const List = () => {
 
+    const [ToDo, setToDo] = useState("");
+    const [ListToDo, setListToDo] = useState("");
+
+
+    const addInput = () => {
+        
+    }
+
     return(
+
         <div className='listContainer'>
             <ul>
-                <li className='inputLi'><input placeholder='What needs to be done?' /><p className='X'>X</p></li>
+                <li className='inputLi'><input type="text" placeholder='What needs to be done?' /><p className='X'></p></li>
                 <li><input className='inputLi2'  /><p className='X'>X</p></li>
                 <li><input className='inputLi2'  /><p className='X'>X</p></li>
-                
-                
-                
+                <button>Click here</button>
             </ul>
         </div>
 
